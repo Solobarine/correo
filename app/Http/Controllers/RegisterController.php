fc7a78fc2dfd87899610b0e569c6bc1a4c75049c
solly <solobarine@gmail.com>
2023-10-30 19:41:19 +0100

    Create Register Controller

---
+++ app/Http/Controllers/RegisterController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class RegisterController extends Controller
+{
+    //
+    public function index()
+    {
+        return view('auth.register');
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|max:255',
+            'username' => 'required|max:255',
+            'email' => 'required|email|max:255',
+            'password' => 'required|confirmed'
+        ]);
+
+        // Check If User Exists
+        $user = User::first('email', $request->email);
+        if ($user) {
+            return back()->with('status', 'User Already Exists');
+        };
+
+        // Create User
+        User::create([
+            'name' => $request->name,
+            'username' => $request->username,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        auth()->attempt($request->only(['email', 'password']));
+
+        return redirect()->route('dashboard');
+    }
+}
