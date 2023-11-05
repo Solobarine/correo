8416e811f88f9422023af13c0bc6cc2011aa828c
solly <solobarine@gmail.com>
2023-11-05 08:23:59 +0100

    Add Dashboard Component.

--- resources/views/components/dashboard.blade.php
+++ resources/views/components/dashboard.blade.php
@@ -58,6 +58,10 @@
             @yield('content')
         </div>
     </div>
+    <footer class="bg-secondary p-3 grid place-items-center">
+        <p class="text-center text-neutral text-md">Copyright &copy;. All Rights Reserved.</p>
+        <p class="text-center text-neutral text-2xl font-semibold">Akpuru Solomon Barine</p>
+    </footer>
     <script>
         // Select Button and Menu Components
         const menuToggle = document.querySelector('#toggle')
